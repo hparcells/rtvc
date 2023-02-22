b8336d71b875b971a09c580a725a615cc010ef1c
Hunter Parcells <hunterparcells@gmail.com>
2023-02-22 15:09:47 -0500

    Initial project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import './globals.css'
-
-export default function RootLayout({
+function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html>
       <head />
       <body>{children}</body>
     </html>
   )
 }
+
+export default RootLayout;
