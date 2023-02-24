fb43f3056e86b8a03fc0d37c46a7cd2eee21ef25
Hunter Parcells <hunterparcells@gmail.com>
2023-02-24 11:40:37 -0500

    Move ElevenLabs logic outside of component.

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -2,15 +2,16 @@
 
 import 'regenerator-runtime';
 
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+import { getAudio, getVoices } from '../../util/elevenlabs';
 
 import classes from './Body.module.scss';
 
 function Body() {
   const [apiKey, setApiKey] = useState<string>('');
-  const [voices, setVoices] = useState<any[]>(null as any);
+  const [voices, setVoices] = useState<any[]>([]);
   const [selectedVoice, setSelectedVoice] = useState<string>('');
   const [audio, setAudio] = useState<string>('');
 
@@ -18,56 +19,30 @@ function Body() {
 
   useEffect(() => {
     (async () => {
-      if (apiKey !== '') {
+      if (apiKey.length === 32) {
         try {
-          const { data } = await axios.get('https://api.elevenlabs.io/v1/voices', {
-            headers: {
-              'xi-api-key': apiKey
-            }
-          });
-          setVoices(data.voices);
+          const voices = await getVoices(apiKey);
+          setVoices(voices);
         } catch {
-          setVoices(null as any);
-          setSelectedVoice(null as any);
+          setVoices([]);
+          setSelectedVoice('');
         }
+        return;
       }
+      setVoices([]);
     })();
   }, [apiKey]);
 
-  async function getAndPlayAudio(text: string) {
-    if (apiKey) {
-      const { data } = await axios.post(
-        `https://api.elevenlabs.io/v1/text-to-speech/${
-          voices.filter((voice) => {
-            return voice.name === selectedVoice;
-          })[0].voice_id
-        }`,
-        {
-          text,
-          voice_settings: {
-            stability: 0.75,
-            similarity_boost: 0.75
-          }
-        },
-        {
-          headers: {
-            'xi-api-key': apiKey
-          },
-          responseType: 'blob'
-        }
-      );
-      const audioUrl = window.URL.createObjectURL(new Blob([data], { type: 'audio/mpeg' }));
-      new Audio(audioUrl).play();
-    }
-  }
-
   useEffect(() => {
     (async () => {
-      if (!listening && transcript !== '' && apiKey !== '' && selectedVoice !== '') {
-        await getAndPlayAudio(transcript);
+      if (!listening) {
+        if (transcript !== '' && apiKey !== '' && selectedVoice !== '') {
+          const audioUrl = await getAudio(selectedVoice, apiKey, transcript);
+          new Audio(audioUrl).play();
+        }
         resetTranscript();
+        SpeechRecognition.startListening();
       }
-      SpeechRecognition.startListening();
     })();
   }, [listening]);
 
@@ -96,21 +71,25 @@ function Body() {
             }}
           />
           <br />
-          <label htmlFor='voice'>Select a Voice</label>
-          <select
-            value={selectedVoice}
-            onChange={(e) => {
-              return setSelectedVoice(e.target.value);
-            }}
-          >
-            {voices?.map((voice) => {
-              return (
-                <option key={voice.id} value={voice.id}>
-                  {voice.name}
-                </option>
-              );
-            })}
-          </select>
+          {voices?.length > 0 && (
+            <>
+              <label htmlFor='voice'>Select a Voice</label>
+              <select
+                value={selectedVoice}
+                onChange={(e) => {
+                  return setSelectedVoice(e.target.value);
+                }}
+              >
+                {voices.map((voice) => {
+                  return (
+                    <option key={voice.name} value={voice.voice_id}>
+                      {voice.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </>
+          )}
         </form>
         <p>Status: {listening ? 'ENABLED' : 'DISABLED'}</p>
         <button onClick={startListening}>Start</button>
---
+++ src/util/elevenlabs.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+export async function getVoices(apiKey: string) {
+  const { data } = await axios.get('https://api.elevenlabs.io/v1/voices', {
+    headers: {
+      'xi-api-key': apiKey
+    }
+  });
+  return data.voices;
+}
+
+/**
+ * Transcribes text using a given voice using the ElevenLabs API and returns a local URL.
+ * @param voiceId The voice ID to generate from.
+ * @param apiKey Your API key found in your ElevenLabs profile.
+ * @param text The text to transcribe.
+ * @returns A local URL to the audio file.
+ */
+export async function getAudio(voiceId: string, apiKey: string, text: string) {
+  const { data } = await axios.post(
+    `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
+    {
+      text,
+      voice_settings: {
+        stability: 0.75,
+        similarity_boost: 0.75
+      }
+    },
+    {
+      headers: {
+        'xi-api-key': apiKey
+      },
+      responseType: 'blob'
+    }
+  );
+  return window.URL.createObjectURL(new Blob([data], { type: 'audio/mpeg' }));
+}
