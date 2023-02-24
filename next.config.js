308f3b449271d79fb0a01bdf859dfcbac224849b
Hunter Parcells <hunterparcells@gmail.com>
2023-02-24 10:59:09 -0500

    SCSS variables and Footer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
 }
 
 module.exports = nextConfig
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,11 @@ import classes from './Footer.module.scss';
 function Footer() {
   return (
     <div className={classes.root}>
-      <p>Footer</p>
+      <span>
+        <a href='https://github.com/hparcells/rtvc' target='_blank'>
+          View Source Code on GitHub
+        </a>
+      </span>
     </div>
   );
 }
