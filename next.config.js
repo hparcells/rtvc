38e5c6c77c70a38f81a1303690de9a26ecf0c4f0
Hunter Parcells <hunterparcells@gmail.com>
2023-02-25 02:50:31 -0500

    Fix production styles issue.

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const path = require('path');
 const nextConfig = {
   experimental: {
     appDir: true
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
   }
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+import 'normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/select/lib/css/blueprint-select.css';
+
 import '../styles/global.scss';
 
 function RootLayout({ children }: { children: React.ReactNode }) {
