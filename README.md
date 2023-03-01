0d8ec52711f33f4f5608e4fb85aec4cff9ad9754
Hunter Parcells <hunterparcells@gmail.com>
2023-03-01 00:03:09 -0500

    Added favicons and meta.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # rtvc
 > Realtime voice cloning using ElevenLabs's API.
 
+![](https://wakatime.com/badge/user/6b7d9181-edde-4a25-857c-e7101bfee7ea/project/418b3b0b-b803-49ea-90c4-d29e7f80eea8.svg?style=for-the-badge)
+
 [https://rtvc.hunterparcells.com/](https://rtvc.hunterparcells.com/)
 
 ## Disclaimer
@@ -13,8 +15,10 @@ There are probably many other *better* solutions that exist than this project, s
 - A subscription to [ElevenLabs](http://www.elevenlabs.io/). Traditionally this is **$5/month** but they currently have an offer for $1 for your first month.
 - If you plan to play the audio through a microphone, I use [VoiceMeeter](https://vb-audio.com/Voicemeeter/) to route my desktop audio to a **virtual mic output**.
 
-# Running
+# Running Locally
 1. Clone or download this repository.
 2. Install needed dependencies with `npm i` with [Node.js](https://nodejs.org/).
 3. Build the app using `npm run build`.
 4. Run using `npm start`.
+
+For development, skip steps 3-4 and instead run `npm run dev`.
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,51 @@
-export default function Head() {
+function Head() {
   return (
     <>
       <title>Realtime Voice Cloning</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+
+      <meta name='keywords' content='elevenlabs,api,voice,cloning' />
+      <meta name='author' content='Hunter Parcells' />
+      <meta name='identifier-url' content='https://rtvc.hunterparcells.com/' />
+      <meta name='msapplication-TileColor' content='#312b41' />
+      <meta name='msapplication-TileImage' content='/icon/favicon-144.png' />
+      <meta name='msapplication-config' content='/icon/browserconfig.xml' />
+
+      <meta
+        name='description'
+        content={`"Realtime" voice transcription and cloning using ElevenLabs's API.`}
+      />
+      <meta
+        property='og:description'
+        content={`"Realtime" voice transcription and cloning using ElevenLabs's API.`}
+      />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://rtvc.hunterparcells.com/' />
+      <meta property='og:image' content='/icon/favicon-310.png' />
+      <meta name='theme-color' content='#312b41' />
+
+      <link rel='shortcut icon' href='/icon/favicon.ico' />
+      <link rel='icon' sizes='16x16 32x32 64x64' href='/icon/favicon.ico' />
+      <link rel='icon' type='image/png' sizes='196x196' href='/icon/favicon-192.png' />
+      <link rel='icon' type='image/png' sizes='160x160' href='/icon/favicon-160.png' />
+      <link rel='icon' type='image/png' sizes='96x96' href='/icon/favicon-96.png' />
+      <link rel='icon' type='image/png' sizes='64x64' href='/icon/favicon-64.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/icon/favicon-32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/icon/favicon-16.png' />
+      <link rel='apple-touch-icon' href='/icon/favicon-57.png' />
+      <link rel='apple-touch-icon' sizes='114x114' href='/icon/favicon-114.png' />
+      <link rel='apple-touch-icon' sizes='72x72' href='/icon/favicon-72.png' />
+      <link rel='apple-touch-icon' sizes='144x144' href='/icon/favicon-144.png' />
+      <link rel='apple-touch-icon' sizes='60x60' href='/icon/favicon-60.png' />
+      <link rel='apple-touch-icon' sizes='120x120' href='/icon/favicon-120.png' />
+      <link rel='apple-touch-icon' sizes='76x76' href='/icon/favicon-76.png' />
+      <link rel='apple-touch-icon' sizes='152x152' href='/icon/favicon-152.png' />
+      <link rel='apple-touch-icon' sizes='180x180' href='/icon/favicon-180.png' />
+      <meta name='msapplication-TileColor' content='#FFFFFF' />
+      <meta name='msapplication-TileImage' content='/icon/favicon-144.png' />
+      <meta name='msapplication-config' content='/icon/browserconfig.xml' />
     </>
   );
 }
+
+export default Head;
